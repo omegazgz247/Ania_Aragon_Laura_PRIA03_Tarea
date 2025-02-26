cb0ace3fca54f0e9190378ad9caea7234785bda5
laura <laniaa@campusdigitalfp.com>
2025-02-26 18:54:41 +0100

    Scripts
    Ajustar acciones
    Creación de observaciones
    Creación de colisiones

---
+++ Assets/Scripts/MoveToTargetAgent.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+
+
+public class MoveToTargetAgent : Agent
+{
+    [SerializeField] private Transform target;
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation((Vector2)transform.localPosition);
+        sensor.AddObservation((Vector2)target.localPosition);
+    }
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveY = actions.ContinuousActions[1];
+        float movementSpeed = 5f;
+        transform.localPosition += new Vector3(moveX, moveY) * Time.deltaTime *
+        movementSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Target target))
+        {
+            AddReward(10f);
+            EndEpisode();
+        }
+        else if (collision.TryGetComponent(out Walls wall))
+        {
+            AddReward(-2f);
+            EndEpisode();
+        }
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(Random.Range(-3.5f, -1.5f),
+        Random.Range(-3.5f, 3.5f));
+        target.localPosition = new Vector3(Random.Range(1.5f, 3.5f),
+        Random.Range(-3.5f, 3.5f));
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+
+}
